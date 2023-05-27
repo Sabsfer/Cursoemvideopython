cbea7b9ed111d487f668c2c8189005ce845ec2c6
Sabsfer <112050409+Sabsfer@users.noreply.github.com>
2023-05-26 21:21:24 -0300

    complementação

--- aula12ex40.py
+++ aula12ex40.py
@@ -5,3 +5,7 @@ print ('Olá, vamos calcular sua média')
 nota1 = float(input('Primeira nota: '))
 nota2 = float(input('Segunda nota: '))
 print ('A primeira nota é {}\nA segunda nota é {}'.format(nota1,nota2))
+media = nota1+nota2/2
+print('Media: {}'.format(media))
+
+
