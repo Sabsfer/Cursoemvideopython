6762333a76d4002989c72f41c6d65288a0c976ad
Sabsfer <112050409+Sabsfer@users.noreply.github.com>
2023-06-27 17:39:20 -0300

    novos codigos

---
+++ aula13.py
@@ -0,0 +1,9 @@
+#for c in range(0,10,3):
+    #print(c)
+#print('FIM')
+s=0
+for c in range (0,4):
+    n=int(input('DIGITE UM NÚMERO: '))
+    s+=n
+print('A soma dos valores são {}'.format(s))
+
---
+++ aula13ex46.py
@@ -0,0 +1,8 @@
+from time import sleep
+print('-'*20)
+print('CONTAGEM REGRESSIVA')
+print('-'*20)
+for c in range (10,-1,-1):
+    sleep(1)
+    print(c)
+print('FELIZ ANO NOVO!!')
